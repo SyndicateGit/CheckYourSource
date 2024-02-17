cd97f8f19204bc4c50da103bfe79321dfb8d364f
Parin <parinparikh1@gmail.com>
2024-02-17 15:57:11 -0500

    graph color

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -4,26 +4,33 @@ import Chart from "chart.js/auto";
 import { Pie } from "react-chartjs-2";
 import "../styles/PieChart.css";
 
-const labels = ["January", "February", "March", "April", "May", "June"];
+const labels = ["coal", "wind", "gas", "hydro", "solar", "nuclear"];
 const data = {
 labels: labels,
 datasets: [
   {
-    label: "My First dataset",
-    /* backgroundColor: "rgb(255, 99, 132)", */
+    label: "energy percentage",
     backgroundColor: ["#007D9C",
     "#244D70",
     "#D123B3",
     "#F7E018",
     "#fff21",
     "#FE452A"],
-    borderColor: "rgb(0,0,255)",
-    data: [0, 10, 5, 20, 30, 45],
+    borderColor: [
+        "rgba(255,99,132,1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)",
+    ],
+    data: [10, 10, 5, 20, 30, 45],
+    borderWidth: 1,
   },
  ]
 };
 const Graph = (props) => {
-    const api_data =props.data;
+    const api_data = props.data;
 
 return (
   <div id="pieChart">
