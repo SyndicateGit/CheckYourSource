fa5a4c312c04fbc602988b25b510b0a58859d7e4
Steven Nguyen <105184434+SdbnCode@users.noreply.github.com>
2024-02-17 14:04:40 -0500

    Search bar done, need to style

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,7 +3,26 @@ import React from 'react';
 const Search = () => {
   return (
     <div>
-      {/* Your code here */}
+      {<div class="search-box">
+        <h1> Explore Energy Usage by Location</h1>
+        <select id="select-province" placeholder="Select a Province">
+            <option value="">Select a Province...</option>
+            <option value="AB">Alberta</option>
+            <option value="BC">British Columbia</option>
+            <option value="MB">Manitoba</option>
+            <option value="NB">New Brunswick</option>
+            <option value="NL">Newfoundland and Labrador</option>
+            <option value="NS">Nova Scotia</option>
+            <option value="ON">Ontario</option>
+            <option value="PE">Prince Edward Island</option>
+            <option value="QC">Quebec</option>
+            <option value="SK">Saskatchewan</option>
+            <option value="NT">Northwest Territories</option>
+            <option value="NU">Nunavut</option>
+            <option value="YT">Yukon</option>
+        </select>
+        <button id="search-button">Search</button>
+    </div>}
     </div>
   );
 };
