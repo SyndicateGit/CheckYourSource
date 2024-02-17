aa32d3e16825066557cf1928cb28d204da77b175
Raymond Zeng <shilei.zeng98@outlook.com>
2024-02-17 14:03:52 -0500

    Lifted fetch data logic to app level.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,28 @@ import './styles/App.css';
 import DisplayData from './components/DisplayData';
 import Search from './components/Search';
 
+import React, { useEffect } from 'react';
+
 function App() {
-  
+    // Used to test display data component.
+  // Actual fetch done in App.js and passed as props to this component.
+  function fetchData() {
+    fetch('https://api.electricitymap.org/v3/power-breakdown/latest?lat=48.8566&lon=2.3522')
+      .then((response) => response.json(), {
+        mode: 'cors',
+        header:{
+          'auth-token': '6jTh4iOxCZaYk',
+        }
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <Search/>
--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,5 +1,6 @@
 // ./components/PieChart.js
 import React from "react";
+import { useEffect } from "react";
 import Chart from "chart.js/auto";
 import { Pie } from "react-chartjs-2";
 const labels = ["January", "February", "March", "April", "May", "June"];
@@ -15,6 +16,23 @@ datasets: [
  ],
 };
 const Graph = () => {
+  function fetchData() {
+    fetch('https://api.electricitymap.org/v3/power-breakdown/latest?lat=48.8566&lon=2.3522')
+      .then((response) => response.json(), {
+        mode: 'cors',
+        header:{
+          'auth-token': '6jTh4iOxCZaYk',
+        }
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
 return (
   <div>
     <Pie data={data} />
