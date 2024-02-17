f9bf010707fb27a318860aa66f36fceff337223f
Raymond Zeng <shilei.zeng98@outlook.com>
2024-02-17 14:16:03 -0500

    Pass data from app to display data component.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import './styles/App.css';
 import DisplayData from './components/DisplayData';
 import Search from './components/Search';
 
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-    // Used to test display data component.
-  // Actual fetch done in App.js and passed as props to this component.
-  function fetchData() {
+  // SEARCH COMPONENT STUFF
+  // State to store the search query
+  const[query, setQuery] = useState('CA-ON');
+
+  // Updates the search query with zone code based on selected zone
+  function submitSearch(e) {
+    e.preventDefault();
+    setQuery(e.target.value);
+  }
+
+  // DISPLAY DATA COMPONENT STUFF
+  // Json data to be passed to display data component
+  const [data, setData] = useState(null);
+
+  // Fetches power breakdown data from the electricity map API based on search zone code.
+  function fetchData(query) {
     fetch('https://api.electricitymap.org/v3/power-breakdown/latest?lat=48.8566&lon=2.3522')
       .then((response) => response.json(), {
         mode: 'cors',
@@ -17,18 +30,20 @@ function App() {
         }
       })
       .then((data) => {
-        console.log(data);
+        setData(data);
       });
   }
 
+  // Rerun api call when query updates
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [query]);
 
   return (
     <div className="App">
       <Search/>
-      <DisplayData/>
+      <DisplayData 
+      data = {data}/>
     </div>
   );
 }
