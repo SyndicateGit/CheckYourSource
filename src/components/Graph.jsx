5d418b0aafe5385f299fd178ca6dc06994a6ceb3
Parin <parinparikh1@gmail.com>
2024-02-17 14:21:03 -0500

    graph update

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,8 +1,7 @@
 // ./components/PieChart.js
 import React from "react";
-import { useEffect } from "react";
 import Chart from "chart.js/auto";
-import { Pie } from "react-chartjs-2";
+import { Pie } from "react-Chart.js-2";
 const labels = ["January", "February", "March", "April", "May", "June"];
 const data = {
 labels: labels,
@@ -16,23 +15,6 @@ datasets: [
  ],
 };
 const Graph = () => {
-  function fetchData() {
-    fetch('https://api.electricitymap.org/v3/power-breakdown/latest?lat=48.8566&lon=2.3522')
-      .then((response) => response.json(), {
-        mode: 'cors',
-        header:{
-          'auth-token': '6jTh4iOxCZaYk',
-        }
-      })
-      .then((data) => {
-        console.log(data);
-      });
-  }
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
 return (
   <div>
     <Pie data={data} />
